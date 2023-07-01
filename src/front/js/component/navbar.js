1f2d7a9ea9d443cddbb0cc43252c7464bc9b9747
Manuel <manusalh@gmail.com>
2023-07-01 06:53:38 +0000

    commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+
+	const logout = () => {
+		localStorage.clear()
+		navigate("/login")
+	}
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
+
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container">
+				<a class="navbar-brand" href="https://manolos05-ideal-xylophone-7q55p7xj9jgcp9g6-3000.preview.app.github.dev/">
+					<img src="https://www.calacademy.org/sites/all/themes/calacademy_zen/images/logo-green-460px.png" alt="Logo" width="30" height="30" class="d-inline-block align-top" />
+					Field Expedition
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Contacto</a>
+						</li>
+					</ul>
+					<ul class="navbar-nav">
+
+						<li class="nav-item">
+							{location.pathname === "/" && <a className="nav-link" href="http://localhost:3001/login">Login</a>}
+						</li>
+
+					</ul>
+				</div>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					{location.pathname !== "/login" && location.pathname !== "/" && <button onClick={logout} className="btn btn-primary">Log Out</button>}
 				</div>
 			</div>
 		</nav>
+
+
 	);
 };
