ad829eea4e4c66b9254658207d9abe07d697ba8e
Manuel <manusalh@gmail.com>
2023-07-02 18:06:36 +0000

    maps cargados en admin view

--- src/front/js/pages/adminViews/createAssign/assignTask.js
+++ src/front/js/pages/adminViews/createAssign/assignTask.js
@@ -5,15 +5,17 @@ import { Context } from "../../../store/appContext";
 export const AssignTask = () => {
 
   const { store, actions } = useContext(Context)
+  const [showModal, setShowModal] = useState(false);
+  const [deleteId, setDeleteId] = useState(null);
 
-  const heading = ["Id", "Proyecto", "Ubicación", "Especie", "Calidad", "Imagen", "Comentatios", "Eliminar"]
+  const heading = ["Proyecto", "id", "area", "Especie", "imagen", "Calidad", "Ubicacion", "imagen ubicacion", "Comentarios", "Eliminar"]
 
   useEffect(() => {
     actions.getSample()
   }, []);
 
 
-  const handledelete = async (id) => {
+  const handleDelete = async (id) => {
     try {
       const response = await fetch(
         `http://localhost:3001/muestra/${id}`,
@@ -45,24 +47,31 @@ export const AssignTask = () => {
         <tbody>
 
           {store.getMuestra.length !== 0 ? (
-            store.getMuestra.map(({ project_name, id, aditional_comments, specimen, image_specimen, quality_specimen, ubication }, i) =>
+            store.getMuestra.map(({ project_name, id, area, aditional_comments, specimen, image_specimen, quality_specimen, ubication, ubication_image }, i) =>
 
               <tr key={i}>
-                <td>{id}</td>
                 <td>{project_name}</td>
-                <td>{ubication}</td>
+                <td>{id}</td>
+                <td>{area}</td>
                 <td>{specimen}</td>
-                <td>{quality_specimen}</td>
                 <td>{image_specimen}</td>
+                <td>{quality_specimen}</td>
+                <td>{ubication}</td>
                 <td>{aditional_comments}</td>
-                <td><button onClick={() => handledelete(id)} className="btn btn-danger">Delete</button></td>
+                <td>{ubication_image}</td>
+                <td><button data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="btn btn-danger" onClick={() => { handleDelete(id) }}>Delete</button></td>
               </tr>
             )
 
           )
-            : (<div></div>)}
+            : (<tr>
+              <td colSpan="10">No hay muestras</td>
+            </tr>)}
         </tbody>
       </table>
+
+
+
 
     </>
 
